a045a748945a3aac2a7c4e07fa01e72c6bba932c
MR Leon Yousif <47602538@students.mq.edu.au>
2026-02-27 10:29:48 +1100

    Screen 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('BingeFlix'),));
+  runApp(const MaterialApp(home: Scaffold(body: Text('Bingeflix'),),));
 }
 
