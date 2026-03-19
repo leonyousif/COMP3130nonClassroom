b5652eb403df4e76529cfff926cd00e2c75d0c86
leonyousif <leon.yousif01@gmail.com>
2026-03-20 02:14:10 +1100

    Week 1 Screen 6

--- Week 1/lib/main.dart
+++ Week 1/lib/main.dart
@@ -4,6 +4,18 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey[300],
+          centerTitle: true,
+          title: const Text(
+            'BINGEWORTHY TV SHOWS',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'BebasNeue',
+              fontSize: 24,
+            ),
+          ),
+        ),
         body: Container(
           color: const Color.fromARGB(255, 0, 150, 136),
           child: const Center(
