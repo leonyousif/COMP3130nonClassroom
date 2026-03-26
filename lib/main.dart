3fca258e0bf3125f34666129a0a47178166aeebb
leonyousif <83793134+leonyousif@users.noreply.github.com>
2026-03-27 10:57:02 +1100

    Week 5 stuff, cant upload full build folder, on seperate computer

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:wk5/expenses.dart';
+import 'package:wk5/models/expense_model.dart';
+
+void main() {
+  runApp(MaterialApp(home: Expenses()));
+}
